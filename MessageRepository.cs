64f945c90cba23e63b861f2c35fc722f95217d6a
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-16 13:06:11 +0000

    Initial commit

---
+++ MessageRepository.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class MessageRepository
+{
+    private readonly IMongoCollection<Message> _messages;
+
+    public MessageRepository(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+        var database = client.GetDatabase("NotificationDB");
+        _messages = database.GetCollection<Message>("Notifications");
+    }
+
+    public async Task<Message> AddMessageAsync(Message message)
+    {
+        await _messages.InsertOneAsync(message);
+        return message;
+    }
+
+    public async Task<List<Message>> GetMessagesAsync(string senderId, string receiverId)
+    {
+        var filter = Builders<Message>.Filter.Where(m =>
+            (m.SenderId == senderId && m.ReceiverId == receiverId) ||
+            (m.SenderId == receiverId && m.ReceiverId == senderId));
+
+        return await _messages.Find(filter).ToListAsync();
+    }
+}
+
+public class Message
+{
+    public ObjectId Id { get; set; }
+    public required string SenderId { get; set; }
+    public required string ReceiverId { get; set; }
+    public required string Content { get; set; }
+    public DateTime Timestamp { get; set; }
+}
+
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Consumer;
+using RabbitMQ.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddSingleton<IConnection>(provider =>
+{
+    try
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = builder.Configuration["RabbitMQ:HostName"],
+            UserName = builder.Configuration["RabbitMQ:UserName"],
+            Password = builder.Configuration["RabbitMQ:Password"],
+        };
+        return factory.CreateConnection();
+    }
+    catch (Exception ex)
+    {
+        // Log locally using the logger
+        var logger = provider.GetRequiredService<ILogger<Program>>(); // Get ILogger from DI container
+        logger.LogError(ex, "RabbitMQ connection failed.");
+
+        // Rethrow the exception to fail the application startup
+        throw new InvalidOperationException("Failed to create RabbitMQ connection.", ex);
+    }
+});
+
+builder.Services.AddSingleton<MessageRepository>();
+builder.Services.AddHostedService<ReadFromQueue>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:4200", "https://ambitious-beach-032f22500.6.azurestaticapps.net")
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .AllowAnyHeader());
+app.MapHub<MySignalRHub>("/listHub");
+app.Run();
